0e8582071d330d269fd53255a1969317ddf50f45
Faik Turan <turanfaik@hotmail.com>
2021-12-10 15:47:10 +0300

    code changed

--- MCD_WindowsFormElemanInceleme/Form1.Designer.cs
+++ MCD_WindowsFormElemanInceleme/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace MCD_WindowsFormElemanInceleme
         {
             this.btnResimSec = new System.Windows.Forms.Button();
             this.chkEkGaranti = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // btnResimSec
@@ -52,11 +53,21 @@ namespace MCD_WindowsFormElemanInceleme
             this.chkEkGaranti.Text = "Ek garanti 2 Yıl";
             this.chkEkGaranti.UseVisualStyleBackColor = true;
             // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(418, 45);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 97);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.chkEkGaranti);
             this.Controls.Add(this.btnResimSec);
             this.Name = "Form1";
@@ -70,6 +81,7 @@ namespace MCD_WindowsFormElemanInceleme
 
         private System.Windows.Forms.Button btnResimSec;
         private System.Windows.Forms.CheckBox chkEkGaranti;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
